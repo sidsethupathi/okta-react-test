5e65f83ce35c1f63b48b20139ecc488109e1454e
Sid Sethupathi <sid.sethupathi@gmail.com>
2021-03-24 23:01:51 -0500

    isAuthenticated override bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, LoginCallback } from '@okta/okta-react';
+import { Security, LoginCallback, AuthService } from '@okta/okta-react';
 import Home from './Home';
 
+const ENABLE_BUG = true;
+
+var authService;
 const config = {
   issuer: 'https://okta.com/auth/default',
   redirectUri: 'http://localhost:3000/implicit/callback',
   clientId: 'foo',
+  isAuthenticated: ENABLE_BUG ? async () => {
+    const accessToken = await authService.getAccessToken();
+    const idToken = await authService.getIdToken();
+    return accessToken && idToken ? true : false;
+  } : null,
 };
+authService = new AuthService(config);
 
 const App = () => { 
   return (
     <Router>
-      <Security {...config}>
+      <Security authService={authService}>
         <Route path='/' exact={true} component={Home}/>
         <Route path='/implicit/callback' component={LoginCallback}/>
       </Security>
